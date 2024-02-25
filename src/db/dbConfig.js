6c0719aedeb35c7b4bdad8b6c661dfdd44594c23
owenrichman <114253776+owenrichman@users.noreply.github.com>
2024-02-24 20:55:02 -0800

    Update dbConfig.js

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'CharlieMojoBlue18!',
+  password: '',
   database: 'ehr_system'
 });
 
